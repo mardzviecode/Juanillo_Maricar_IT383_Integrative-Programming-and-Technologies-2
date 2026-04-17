615521c25bc1788485f54053706d3f69aac0aaed
mardzviecode <maricarjuanillo4@gmail.com>
2026-04-17 16:56:07 +0800

    Activity 2

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './login/login';
 import { DashboardComponent } from './dashboard/dashboard';
+import { PortfolioComponent } from './portfolio/portfolio';
 
 export const routes: Routes = [
   { path: '', component: LoginComponent },
   { path: 'dashboard', component: DashboardComponent },
+  { path: 'portfolio', component: PortfolioComponent },
   { path: '**', redirectTo: '' }
 ];
